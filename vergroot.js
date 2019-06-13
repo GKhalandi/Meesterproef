0dbbb74b7b78b18f3f38ef55cd7872930e698d0c
Gizing Khalandi <gzing20@me.com>
2019-06-13 18:42:59 +0200

    Scroll water doet het, javascript reageert slecht

---
+++ vergroot.js
@@ -0,0 +1,7 @@
+// var inzoom = document.querySelector('.binnenkant');
+//
+// // inzoom.addEventListener('click', function(){
+// // 				console.log('clickhand');
+// // });
+// //
+console.log('test');
